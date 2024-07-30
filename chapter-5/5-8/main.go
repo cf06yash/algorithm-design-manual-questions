3d7ab42fc01e56ef3221500768f0a31480e92286
yash <cf06.yash@gmail.com>
2024-07-30 18:29:09 +0530

    find median sorted arrays

--- chapter-5/5-8/main.go
+++ chapter-5/5-8/main.go
@@ -2,44 +2,111 @@ package main
 
 import "fmt"
 
-func find(arr []int, l, r, key int) int {
-	if l >= r {
-		return l
+func findMedianOneArray(arr []int, l int) float64 {
+	x := getPositionsToFind(0, l)
+	sum := float64(0)
+	for i := 0; i < len(x); i++ {
+		sum += float64(arr[x[i]])
 	}
-	mid := (l + r) / 2
-	if arr[mid] > key {
-		return find(arr, l, mid-1, key)
+
+	return sum / float64(len(x))
+}
+
+func findPositionsOfElement(arr []int, response *[]int, l, r, key int) {
+	if l >= r {
+		if arr[l] < key {
+			*response = append(*response, l+1)
+		} else if arr[l] == key {
+			*response = append(*response, l)
+			*response = append(*response, l+1)
+		} else {
+			*response = append(*response, l)
+		}
 	} else {
-		return find(arr, mid+1, r, key)
+		mid := (l + r) / 2
+		if arr[mid] == key {
+			*response = append(*response, mid)
+			findPositionsOfElement(arr, response, l, mid-1, key)
+			findPositionsOfElement(arr, response, mid+1, r, key)
+		} else if arr[mid] > key {
+			findPositionsOfElement(arr, response, l, mid-1, key)
+		} else {
+			findPositionsOfElement(arr, response, mid+1, r, key)
+		}
 	}
 }
 
-func helper(a, b []int, med, l, r int) int {
-	if l <= r {
+func getMedian(arr1 []int, arr2 []int, pos int) int {
+	l := 0
+	r := len(arr1) - 1
+
+	for l <= r {
+		response := []int{}
 		mid := (l + r) / 2
-		posInA := find(a, 0, len(a)-1, b[mid])
-		if posInA+mid == med {
-			return mid
-		} else if posInA+mid < med {
-			return helper(a, b, med, mid+1, r)
+		findPositionsOfElement(arr2, &response, 0, len(arr2)-1, arr1[mid])
+		fmt.Printf("response for element %d is %v\n", arr1[mid], response)
+		for i := 0; i < len(response); i++ {
+			if response[i]+mid == pos {
+				return mid
+			}
+		}
+
+		x := response[len(response)-1]
+		if x+mid < pos {
+			l = mid + 1
 		} else {
-			return helper(a, b, med, l, mid-1)
+			r = mid - 1
 		}
 	}
+
 	return -1
 }
 
-func findMedian(a, b []int, med int) int {
-	m := len(b)
-	n := len(a)
-	x := helper(a, b, med, 0, m-1)
-
-	if x == -1 {
-		x = helper(b, a, med, 0, n-1)
-		return a[x]
-	} else {
-		return b[x]
+func findPos(arr1 []int, arr2 []int, pos int) float64 {
+	idx := getMedian(arr1, arr2, pos)
+	if idx != -1 {
+		return float64(arr1[idx])
 	}
+	idx = getMedian(arr2, arr1, pos)
+	return float64(arr2[idx])
+}
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	n := len(nums1)
+	m := len(nums2)
+
+	if n == 0 && m == 0 {
+		return 0
+	}
+	if n == 0 {
+		return findMedianOneArray(nums2, m)
+	} else if m == 0 {
+		return findMedianOneArray(nums1, n)
+	}
+
+	x := getPositionsToFind(n, m)
+	sum := float64(0)
+	for i := 0; i < len(x); i++ {
+		sum += findPos(nums1, nums2, x[i])
+	}
+
+	return sum / float64(len(x))
+}
+
+func getPositionsToFind(n, m int) []int {
+	x := []int{}
+	if n+m == 0 {
+		return x
+	}
+	total := n + m
+	if total%2 == 0 {
+		a := (total / 2) - 1
+		x = append(x, a)
+		x = append(x, a+1)
+	} else {
+		x = append(x, total/2)
+	}
+	return x
 }
 
 func main() {
@@ -49,24 +116,7 @@ func main() {
 
 	a := []int{1, 2, 3, 4, 5, 6, 7, 23, 56, 99, 100}
 	b := []int{2, 3, 4, 5, 8, 11, 17}
-	n := len(a)
-	m := len(b)
-	fmt.Printf("a %v b %v\n", a, b)
-	fmt.Printf("a %d b %d\n", n, m)
-	if (m+n)%2 == 0 {
-		med1 := (m + n) / 2
-		med2 := med1 + 1
-		median1 := findMedian(a, b, med1)
-		fmt.Printf("median 1 element %d\n", median1)
-
-		median2 := findMedian(a, b, med2)
-		fmt.Printf("median 2 element %d\n", median2)
-
-		fmt.Printf("final median %f\n", (float64(median1)+float64(median2))/2)
-	} else {
-		med := (m + n) / 2
-		median := findMedian(a, b, med)
-		fmt.Printf("median element %d\n", median)
-	}
+	median := findMedianSortedArrays(a, b)
+	fmt.Printf("median %f\n", median)
 
 }
